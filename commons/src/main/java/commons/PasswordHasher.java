87b8ffd0ce8314667aeb2547a950dfbc81e577c7
maruxandei <aruxandei@student.tudelft.nl>
2024-02-21 08:54:45 +0100

    Added password hasher

---
+++ commons/src/main/java/commons/PasswordHasher.java
@@ -0,0 +1,31 @@
+package commons;
+
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+public class PasswordHasher {
+    /*
+        Uses SHA-256 algorithm to encrypt the password for saving in the database
+     */
+    private final MessageDigest encrypt;
+    public PasswordHasher () {
+        try {
+            this.encrypt = MessageDigest.getInstance("SHA-256");
+        } catch (NoSuchAlgorithmException e) {
+            throw new RuntimeException("Error hashing password", e);
+        }
+    }
+    public String compute(String password) {
+        byte[] encodedHash = encrypt.digest(password.getBytes(StandardCharsets.UTF_8));
+        StringBuilder encrypted = new StringBuilder(2 * encodedHash.length);
+        for (byte b: encodedHash) {
+            String hex = Integer.toHexString(0xff & b);
+            if (hex.length() == 1) {
+                encrypted.append('0');
+            }
+            encrypted.append(hex);
+        }
+        return encrypted.toString();
+    }
+}
