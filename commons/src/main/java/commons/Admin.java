a27c7f3634552ec01f8f948d30f4271e7e1e4424
maruxandei <aruxandei@student.tudelft.nl>
2024-02-21 09:30:01 +0100

    Fixed failing pipeline

--- commons/src/main/java/commons/Admin.java
+++ commons/src/main/java/commons/Admin.java
@@ -1,10 +1,6 @@
 package commons;
 
 import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-
-import java.nio.charset.StandardCharsets;
 
 import jakarta.persistence.Id;
 import org.apache.commons.lang3.builder.EqualsBuilder;
