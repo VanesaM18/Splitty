93296e7afaa55c0007c6dea24938df7522902a8e
Vanesa Mitseva <v.p.mitseva-1@student.tudelft.nl>
2024-02-21 19:13:39 +0200

    Fixing pipeline failiure

--- client/src/main/java/client/scenes/AddParticipantsCtrl.java
+++ client/src/main/java/client/scenes/AddParticipantsCtrl.java
@@ -3,8 +3,6 @@ package client.scenes;
 import client.utils.ServerUtils;
 import com.google.inject.Inject;
 import commons.Participant;
-import commons.Person;
-import commons.Quote;
 import jakarta.ws.rs.WebApplicationException;
 import javafx.fxml.FXML;
 import javafx.scene.control.Alert;
