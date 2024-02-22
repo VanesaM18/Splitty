bb6f3b347a789a7b39b0627107caf08015a856e8
maruxandei <aruxandei@student.tudelft.nl>
2024-02-22 09:50:04 +0100

    Fixed pipeline for tests

--- commons/src/test/java/commons/AdminTest.java
+++ commons/src/test/java/commons/AdminTest.java
@@ -8,7 +8,7 @@ class AdminTest {
     @Test
     public void getters() {
         Admin ad = new Admin("mat50013", "Ca_01234535", "stefmatei22@gmail.com");
-        assertEquals("George", ad.getUsername());
+        assertEquals("mat50013", ad.getUsername());
         assertEquals("stefmatei22@gmail.com", ad.getEmail());
         PasswordHasher tst = new PasswordHasher();
         assertEquals(tst.compute("Ca_01234535"), ad.getPassword());
