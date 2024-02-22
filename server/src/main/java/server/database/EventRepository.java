9c3baa9687067eddc4d544d8b294a915de4f5d18
Ruben van der Giessen <R.H.vanderGiessen@student.tudelft.nl>
2024-02-22 21:31:03 +0100

    Create event repository and add API endpoints for getting and creating events

---
+++ server/src/main/java/server/database/EventRepository.java
@@ -0,0 +1,12 @@
+package server.database;
+
+import commons.Event;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface EventRepository extends JpaRepository<Event, Long> {
+
+    boolean existsByInviteCodeEqualsIgnoreCase(String inviteCode);
+
+    Event findFirstByInviteCodeEqualsIgnoreCase(String inviteCode);
+
+}
