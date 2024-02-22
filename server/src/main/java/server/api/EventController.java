4af8c80b1bf2c6186849b655b24d91830d239029
Ruben van der Giessen <R.H.vanderGiessen@student.tudelft.nl>
2024-02-22 21:40:37 +0100

    Fix NPE

--- server/src/main/java/server/api/EventController.java
+++ server/src/main/java/server/api/EventController.java
@@ -44,7 +44,7 @@ public class EventController {
         // NOTE: The participant list must be empty, people can only be added to an event by using
         // the invite code.
         if (isNullOrEmpty(event.getName()) || event.getDateTime() == null
-                || event.getParticipants() != null || !event.getParticipants().isEmpty()) {
+                || (event.getParticipants() != null && !event.getParticipants().isEmpty())) {
             return ResponseEntity.badRequest().build();
         }
 
