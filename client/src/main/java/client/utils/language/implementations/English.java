f5483ce4aa5fb06d45f88e5f5667b072e780492d
Andreea Grigoraș <A.Grigoras-1@student.tudelft.nl>
2024-02-26 19:35:10 +0000

    core languages

--- client/src/main/java/client/utils/language/implementations/English.java
+++ client/src/main/java/client/utils/language/implementations/English.java
@@ -2,31 +2,28 @@ package client.utils.language.implementations;
 
 import client.Main;
 import client.utils.language.Language;
+import client.utils.language.LanguageTemplate;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.scene.control.Button;
 import java.io.IOException;
 import java.util.Locale;
 
-public class English implements Language {
+public class English extends LanguageTemplate {
+
+
     @Override
-    public Button getButton() {
-        Button button = new Button();
-        button.setText("English");
-        button.setOnAction(new EventHandler<ActionEvent>() {
-            @Override
-            public void handle(ActionEvent event) {
-                var optionalMain = Main.getInstance();
-                if (optionalMain.isPresent()) {
-                    try {
-                        optionalMain.get().updateLocale(new Locale("en", "EN"));
-                    } catch (IOException e) {
-                        //TODO log error
-                    }
-                }
-            }
-        });
-        return button;
+    protected String getText() {
+        return "English";
     }
 
+    @Override
+    protected String getLanguage() {
+        return "en";
+    }
+
+    @Override
+    protected String getCountry() {
+        return "EN";
+    }
 }
