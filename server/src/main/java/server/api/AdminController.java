ac2f6aa3fc768489877a6a0bbf2a995672446d15
maruxandei <aruxandei@student.tudelft.nl>
2024-02-28 11:42:47 +0100

    Fix reverted changes

--- server/src/main/java/server/api/AdminController.java
+++ server/src/main/java/server/api/AdminController.java
@@ -21,6 +21,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -54,12 +55,22 @@ public class AdminController {
     @PostMapping(path = { "", "/" })
     public ResponseEntity<Admin> add(@RequestBody Admin admin) {
 
-        if (admin.username == null || repo.existsById(admin.username) || isNullOrEmpty(admin.email)) {
+        if (admin.username == null || repo.existsById(admin.username)) {
             return ResponseEntity.badRequest().build();
         }
 
         Admin saved = repo.save(admin);
         return ResponseEntity.ok(saved);
+    }
+
+
+    @DeleteMapping("/{username}")
+    public ResponseEntity<String> delete(@PathVariable("username") String username) {
+        if (isNullOrEmpty(username) || !repo.existsById(username)) {
+            return ResponseEntity.badRequest().body("Can't delete the admin account");
+        }
+        repo.deleteById(username);
+        return ResponseEntity.ok().body("Deleted successfully");
     }
 
     @PostMapping("login")
