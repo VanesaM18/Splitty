a5b6b8362c7fad0d732970eba9725865dde0057a
Vanesa Mitseva <v.p.mitseva-1@student.tudelft.nl>
2024-02-29 21:33:44 +0200

    Add function added.

--- server/src/main/java/server/WebSocketHandler.java
+++ server/src/main/java/server/WebSocketHandler.java
@@ -1,9 +1,6 @@
 package server;
 
-import commons.Admin;
-import commons.Event;
-import commons.Quote;
-import commons.WebSocketMessage;
+import commons.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
@@ -14,6 +11,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.websocket.server.ServerEndpoint;
 import server.api.AdminController;
 import server.api.EventController;
+import server.api.ParticipantController;
 import server.api.QuoteController;
 
 import java.util.List;
@@ -29,6 +27,8 @@ public class WebSocketHandler extends TextWebSocketHandler {
     private EventController eventController;
     @Autowired
     private QuoteController quoteController;
+    @Autowired
+    private ParticipantController participantController;
 
     /**
      * Creates a class for handling the websocket connection
@@ -64,6 +64,8 @@ public class WebSocketHandler extends TextWebSocketHandler {
             handleEventsApi(session, request);
         } else if (endPoint.contains("/quotes")) {
             handleQuotesApi(session, request);
+        } else if (endPoint.contains("/participants")) {
+            handleParticipantsApi (session, request);
         }
     }
     /**
@@ -153,6 +155,33 @@ public class WebSocketHandler extends TextWebSocketHandler {
                     Admin admin = objectMapper.convertValue(request.getData(), Admin.class);
                     ResponseEntity<String> status = adminController.login(admin);
                     this.returnResult(session, request, status.getBody());
+                }
+            }
+        }
+    }
+
+    /**
+     * Handles the participants specific to /api/participants
+     * @param session the channel used to communicate
+     * @param request the parsed request
+     * @throws Exception if the message can't be parsed
+     */
+    private void handleParticipantsApi(WebSocketSession session,
+                                 WebSocketMessage request) throws Exception {
+        switch (request.getEndpoint()) {
+            case "api/participants" -> {
+                if ("GET".equals(request.getMethod())) {
+                    List<Participant> all = participantController.getAll();
+                    this.returnResult(session, request, all);
+                } else if ("POST".equals(request.getMethod())) {
+                    Participant p = objectMapper.convertValue(request.getData(), Participant.class);
+                    ResponseEntity<Participant> savedEvent = participantController.add(p);
+                    this.returnResult(session, request, savedEvent.getBody());
+                }
+            }
+            case "api/participants/id" -> {
+                if ("DELETE".equals(request.getMethod())) {
+                    // TO DO
                 }
             }
         }
---
+++ server/src/main/java/server/api/ParticipantController.java
@@ -0,0 +1,70 @@
+package server.api;
+
+import commons.Participant;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import server.database.ParticipantRepository;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/participants")
+public class ParticipantController {
+    private final ParticipantRepository repo;
+
+    /**
+     * Create a new participant controller.
+     * This controller contains all api endpoints that have to do with participants.
+     *
+     * @param repo The repository used for creating, reading, updating and deleting participants.
+     */
+    public ParticipantController(ParticipantRepository repo) {
+        this.repo = repo;
+    }
+
+    /**
+     * Gets all participant rows in our database
+     * @return the list of admins
+     */
+    @GetMapping(path = { "", "/" })
+    public List<Participant> getAll() {
+        return repo.findAll();
+    }
+
+    /**
+     * Adds a participant.
+     * @param participant the participant to be added
+     * @return the added admin
+     */
+    @PostMapping(path = { "", "/" })
+    public ResponseEntity<Participant> add(@RequestBody Participant participant) {
+        if (participant.getName() == null) {
+            return ResponseEntity.badRequest().build();
+        }
+
+        Participant saved = repo.save(participant);
+        return ResponseEntity.ok(saved);
+    }
+
+    /**
+     * Deletes a participant if is exits by id.
+     * @param id the id to be deleted
+     * @return the status of the deletion
+     */
+    @DeleteMapping("/{id}")
+    public ResponseEntity<String> delete(@PathVariable("id") Long id) {
+        if (isNull(id) || !repo.existsById(id)) {
+            return ResponseEntity.badRequest().body("Can't delete the participant.");
+        }
+        repo.deleteById(id);
+        return ResponseEntity.ok().body("Deleted successfully");
+    }
+
+    /**
+     *  Checks if a Long is null.
+     */
+    private static boolean isNull(Long s) {
+        return s == null;
+    }
+
+}
