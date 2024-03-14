366690a7b2aeca410d4275c06b9f5a4f52921c4e
maruxandei <aruxandei@student.tudelft.nl>
2024-03-14 19:23:43 +0100

    Fix pipeline

--- server/src/test/java/server/AdminControllerTest.java
+++ server/src/test/java/server/AdminControllerTest.java
@@ -21,7 +21,7 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @WebMvcTest(AdminController.class)
-public class AdminTest {
+public class AdminControllerTest {
     @Autowired
     private MockMvc mockMvc;
 
