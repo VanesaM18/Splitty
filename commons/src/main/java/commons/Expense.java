9546e9f612a2cfdffa67d7e77173e80e1e9480c2
Nathan Huisman <t.n.huisman@student.tudelft.nl>
2024-03-15 19:14:10 +0100

    feat(expenses): Add split between, date and tests

--- commons/src/main/java/commons/Expense.java
+++ commons/src/main/java/commons/Expense.java
@@ -1,5 +1,14 @@
 package commons;
 
+import static org.apache.commons.lang3.builder.ToStringStyle.MULTI_LINE_STYLE;
+
+import java.util.Date;
+import java.util.Set;
+
+import org.apache.commons.lang3.builder.EqualsBuilder;
+import org.apache.commons.lang3.builder.HashCodeBuilder;
+import org.apache.commons.lang3.builder.ToStringBuilder;
+
 import jakarta.persistence.*;
 
 @Entity
@@ -9,19 +18,24 @@ public class Expense {
     private Long id;
     @Embedded
     @AttributeOverrides({
-        @AttributeOverride(name = "value", column = @Column(name = "amount_value")),
-        @AttributeOverride(name = "currency", column = @Column(name = "amount_currency")),
-        @AttributeOverride(name = "fractionDivider",
-                column = @Column(name = "amount_fraction_divider"))
+            @AttributeOverride(name = "value", column = @Column(name = "amount_value")),
+            @AttributeOverride(name = "currency", column = @Column(name = "amount_currency")),
+            @AttributeOverride(name = "fractionDivider", column = @Column(name = "amount_fraction_divider"))
     })
     private Monetary amount;
     @ManyToOne(optional = false)
     @JoinColumn(name = "PARTICIPANT_ID", referencedColumnName = "id")
-    private Participant participant;
+    private Participant creator;
 
     @ManyToOne(optional = false)
     @JoinColumn(name = "event_id", nullable = false)
     private Event event;
+
+    private Date date;
+
+    @ManyToMany
+    @JoinTable(name = "EXPENSE_PARTICIPANTS")
+    private Set<Participant> splitBetween;
 
     /**
      * For object mapper
@@ -30,7 +44,77 @@ public class Expense {
     }
 
     /**
+     * Create a new expense
+     * 
+     * @param event   The event associated with this expense
+     * @param creator The participant that made this expense
+     * @param amount  The amount of this expense
+     * @param date    The date this expense took place
+     */
+    public Expense(Event event, Participant creator, Monetary amount, Date date, Set<Participant> splitBetween) {
+        this.event = event;
+        this.creator = creator;
+        this.amount = amount;
+        this.date = date;
+        if (splitBetween == null) {
+            this.splitBetween = Set.of();
+        } else {
+            this.splitBetween = splitBetween;
+        }
+    }
+
+    /**
+     * Gets the participants who will split the bill
+     * 
+     * @return The participants that will split the bill
+     */
+    public Set<Participant> getSplitBetween() {
+        return splitBetween;
+    }
+
+    /**
+     * Sets the participants who will split the bill
+     * 
+     * @param splitBetween The participants that will split the bill
+     *
+     */
+    public void setSplitBetween(Set<Participant> splitBetween) {
+        this.splitBetween = splitBetween;
+    }
+
+    /**
+     * Adds a participant to the set of participants.
+     * WARN: If the expense is constructed with an immutable set, this will error
+     *
+     * @param participant Add to the list of participants that will split the bill
+     * 
+     * @return Whether the participant was allready added to the list
+     */
+    public boolean addParticipant(Participant participant) {
+        return this.splitBetween.add(participant);
+    }
+
+    /**
+     * Get the date associated with this event.
+     * 
+     * @return The date
+     */
+    public Date getDate() {
+        return date;
+    }
+
+    /**
+     * Set the date for this event.
+     * 
+     * @param date the date to set
+     */
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    /**
      * Get the event associated with this expense
+     * 
      * @return The event
      */
     public Event getEvent() {
@@ -39,6 +123,7 @@ public class Expense {
 
     /**
      * Set the event associated to this expense
+     * 
      * @param event The event to set
      */
     public void setEvent(Event event) {
@@ -47,6 +132,7 @@ public class Expense {
 
     /**
      * Get the monetary amount
+     * 
      * @return The amount
      */
     public Monetary getAmount() {
@@ -55,6 +141,7 @@ public class Expense {
 
     /**
      * Set the associated amount
+     * 
      * @param amount The associated amount
      */
     public void setAmount(Monetary amount) {
@@ -63,22 +150,25 @@ public class Expense {
 
     /**
      * Get the associated participant
+     * 
      * @return The associated participant
      */
-    public Participant getParticipant() {
-        return participant;
+    public Participant getCreator() {
+        return creator;
     }
 
     /**
      * Set the associated participant
+     * 
      * @param participant The associated participant
      */
-    public void setParticipant(Participant participant) {
-        this.participant = participant;
+    public void setCreator(Participant participant) {
+        this.creator = participant;
     }
 
     /**
      * Set the id
+     * 
      * @param id THe id to set
      */
     public void setId(Long id) {
@@ -87,6 +177,7 @@ public class Expense {
 
     /**
      * Get the id
+     * 
      * @return the id
      */
     public Long getId() {
@@ -94,14 +185,34 @@ public class Expense {
     }
 
     /**
-     * Create a new expense
-     * @param event The event associated with this expense
-     * @param participant The participant that made this expense
-     * @param amount The amount of this expense
+     * Checks if another object is exactly the same as this one.
+     * 
+     * @param obj Object that needs to be checked.
+     * @return Weather the given object is equal to this one.
      */
-    public Expense(Event event, Participant participant, Monetary amount) {
-        this.event = event;
-        this.participant = participant;
-        this.amount = amount;
+    @Override
+    public boolean equals(Object obj) {
+        return EqualsBuilder.reflectionEquals(this, obj);
     }
+
+    /**
+     * Computes a hash code for this object.
+     * 
+     * @return A hash code value for this object.
+     */
+    @Override
+    public int hashCode() {
+        return HashCodeBuilder.reflectionHashCode(this);
+    }
+
+    /**
+     * Turns the quote into a string.
+     * 
+     * @return String representation of the object.
+     */
+    @Override
+    public String toString() {
+        return ToStringBuilder.reflectionToString(this, MULTI_LINE_STYLE);
+    }
+
 }
