90af510ecfaaa324bd4c41bee1fa429740c5a211
Noah Swartjes <N.O.Swartjes@student.tudelft.nl>
2024-03-17 20:41:08 +0100

    Edited fxml for open debts

--- client/src/main/java/client/scenes/OpenDebtsCtrl.java
+++ client/src/main/java/client/scenes/OpenDebtsCtrl.java
@@ -2,23 +2,25 @@ package client.scenes;
 
 import com.google.inject.Inject;
 import javafx.fxml.FXML;
+import javafx.scene.control.Accordion;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
-import javafx.scene.control.Label;
+import javafx.scene.control.TitledPane;
 import javafx.scene.layout.AnchorPane;
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.URL;
+
+//import java.io.BufferedReader;
+//import java.io.InputStreamReader;
+//import java.net.HttpURLConnection;
+//import java.net.URL;
 
 public class OpenDebtsCtrl {
     @FXML
     private AnchorPane anchorPane;
     @FXML
-    private Label totalDebtLabel;
+    private Accordion debtAccordion;
     private final MainCtrl mainCtrl;
     @FXML
-    private Button clearDebtButton;
+    private Button markReceived;
 
     /**
      * constructs open debts
@@ -34,43 +36,43 @@ public class OpenDebtsCtrl {
      */
     public void initialize() {
 
-        try {
-            URL url = new URL("http://localhost:8080/debts/event");
-            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
-            connection.setRequestMethod("GET");
-
-            BufferedReader reader = new BufferedReader(new InputStreamReader(connection
-                    .getInputStream()));
-            StringBuilder response = new StringBuilder();
-            String line;
-            while ((line = reader.readLine()) != null) {
-                response.append(line);
-            }
-            reader.close();
-
-            String totalDebt = response.toString();
-            totalDebtLabel.setText(totalDebt);
-            createClearDebtButton();
-
-            connection.disconnect();
-        } catch (Exception e) {
-            showErrorDialog("Failed to retrieve total debt. Please try again later.");
-            e.printStackTrace();
-        }
+//        try {
+//            URL url = new URL("http://localhost:8080/debts/event");
+//            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+//            connection.setRequestMethod("GET");
+//
+//            BufferedReader reader = new BufferedReader(new InputStreamReader(connection
+//                    .getInputStream()));
+//            StringBuilder response = new StringBuilder();
+//            String line;
+//            while ((line = reader.readLine()) != null) {
+//                response.append(line);
+//            }
+//            reader.close();
+//
+//            String totalDebt = response.toString();
+//            totalDebtLabel.setText(totalDebt);
+//            createClearDebtButton();
+//
+//            connection.disconnect();
+//        } catch (Exception e) {
+//            showErrorDialog("Failed to retrieve total debt. Please try again later.");
+//            e.printStackTrace();
+//        }
 
     }
-    private void createClearDebtButton() {
-        clearDebtButton = new Button("Clear Total Debt");
-        clearDebtButton.setLayoutX(200);
-        clearDebtButton.setLayoutY(200);
-        clearDebtButton.setOnAction(event -> clearTotalDebt());
-        anchorPane.getChildren().add(clearDebtButton);
-    }
+//    private void createClearDebtButton() {
+//        clearDebtButton = new Button("Clear Total Debt");
+//        clearDebtButton.setLayoutX(200);
+//        clearDebtButton.setLayoutY(200);
+//        clearDebtButton.setOnAction(event -> clearTotalDebt());
+//        anchorPane.getChildren().add(clearDebtButton);
+//    }
 
     @FXML
     private void clearTotalDebt() {
-        anchorPane.getChildren().remove(totalDebtLabel);
-        anchorPane.getChildren().remove(clearDebtButton);
+        anchorPane.getChildren().remove(debtAccordion);
+        anchorPane.getChildren().remove(markReceived);
     }
 
     private void showErrorDialog(String message) {
