44286fc53fb6854f2bce2eaeaefc28efb9af67b3
Noah Swartjes <N.O.Swartjes@student.tudelft.nl>
2024-03-19 12:47:06 +0100

    updated UI openDebts

--- client/src/main/java/client/scenes/OpenDebtsCtrl.java
+++ client/src/main/java/client/scenes/OpenDebtsCtrl.java
@@ -8,6 +8,7 @@ import javafx.fxml.FXML;
 import javafx.scene.Node;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.ScrollPane;
 import javafx.scene.control.TitledPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
@@ -20,6 +21,8 @@ public class OpenDebtsCtrl {
     private final ServerUtils server;
     @FXML
     private VBox debtContainer;
+    @FXML
+    private ScrollPane scrollPane;
 
     /**
      * constructs open debts
@@ -51,36 +54,42 @@ public class OpenDebtsCtrl {
         List<Debt> list = Event.paymentsToDebt(e);
 
         for (Debt debt : list) {
-            HBox hbox = new HBox();
-            String hboxId = "hbox_" + debt.getId();
-            hbox.setId(hboxId);
-            TitledPane titledPane = new TitledPane();
-            titledPane.setText(debt.getCreditor().getName() + " gives "
-                    + debt.getAmount().getInternalValue()
-                    + " to " + debt.getDebtor().getName());
+            if(debt.getDebtor().getId() != debt.getCreditor().getId()) {
+                HBox hbox = new HBox();
+                String hboxId = "hbox_" + debt.getId();
+                hbox.setId(hboxId);
+                TitledPane titledPane = new TitledPane();
+                titledPane.setExpanded(false);
+                titledPane.setText(debt.getCreditor().getName() + " gives "
+                        + debt.getAmount().getInternalValue()
+                        + " to " + debt.getDebtor().getName());
 
-            VBox content = new VBox();
-            Label debtLabel = new Label( "Bank information available, transfer the money to:\n"
-                    + "Account holder: " + debt.getDebtor().getName()
-                    + "\nIBAN: " + debt.getDebtor().getIban()
-                    + "\nBIC: " + debt.getDebtor().getBic());
-            content.getChildren().add(debtLabel);
-            titledPane.setContent(content);
+                VBox content = new VBox();
+                Label debtLabel = new Label("Bank information available, transfer the money to:\n"
+                        + "Account holder: " + debt.getDebtor().getName()
+                        + "\nIBAN: " + debt.getDebtor().getIban()
+                        + "\nBIC: " + debt.getDebtor().getBic());
+                content.getChildren().add(debtLabel);
+                titledPane.setContent(content);
 
-            Button markReceivedButton = new Button("Mark Received");
-            markReceivedButton.setOnAction(event -> {
+                Button markReceivedButton = new Button("Mark Received");
+                markReceivedButton.setOnAction(event -> {
 //                server.removeDebt(debt.getId());
-                Node parentHBox = markReceivedButton.getParent();
-                if (parentHBox instanceof HBox) {
-                    debtContainer.getChildren().remove(parentHBox);
-                }
-            });
+                    Node parentHBox = markReceivedButton.getParent();
+                    if (parentHBox instanceof HBox) {
+                        debtContainer.getChildren().remove(parentHBox);
+                    }
+                });
 
-            hbox.getChildren().addAll(titledPane, markReceivedButton);
-            HBox.setMargin(titledPane, new Insets(10, 0, 0, 0));
-            HBox.setMargin(markReceivedButton, new Insets(10, 0, 0, 10));
-            debtContainer.getChildren().add(hbox);
+                hbox.getChildren().addAll(titledPane, markReceivedButton);
+                HBox.setMargin(titledPane, new Insets(10, 0, 0, 0));
+                HBox.setMargin(markReceivedButton, new Insets(10, 0, 0, 10));
+                debtContainer.getChildren().add(hbox);
+            }
         }
+        scrollPane.setContent(debtContainer); // Set the VBox as the content of the ScrollPane
+        scrollPane.setFitToWidth(true); // Adjusts the width of the scroll pane to its content
+        scrollPane.setFitToHeight(true);
     }
 
     /**
