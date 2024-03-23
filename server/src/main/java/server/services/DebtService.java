f7d62796ffb6adfb9d65d55e9d54aa04cef233c7
Andreea Grigoraș <A.Grigoras-1@student.tudelft.nl>
2024-03-23 19:48:51 +0000

    implement debt service

--- server/src/main/java/server/services/DebtService.java
+++ server/src/main/java/server/services/DebtService.java
@@ -1,8 +1,80 @@
 package server.services;
 
+import commons.Debt;
 import org.springframework.stereotype.Service;
+import server.database.DebtRepository;
+
+import java.util.Optional;
+import java.util.Random;
 
 @Service
 public class DebtService {
+    private final DebtRepository debtRepository;
 
+    /**
+     * constructor for DebtService
+     * @param debtRepository repository for accessing debt data
+     */
+    public DebtService(DebtRepository debtRepository) {
+        this.debtRepository = debtRepository;
+    }
+
+    /**
+     * gets all debts from the repository
+     * @return list of all debts
+     */
+    public java.util.List<commons.Debt> getAllDebts() {
+        return debtRepository.findAll();
+    }
+
+    /**
+     * gets a debt by its ID
+     * @param id ID of the debt to retrieve
+     * @return an Optional containing the debt if found, otherwise empty
+     */
+    public Optional<commons.Debt> getDebtById(long id) {
+        if (id < 0 || !debtRepository.existsById(id)) {
+            return Optional.empty();
+        }
+        return debtRepository.findById(id);
+    }
+
+    /**
+     * deletes a debt by its ID
+     * @param id ID of the debt to delete
+     * @return an Optional containing the deleted debt if found and deleted, otherwise empty
+     */
+    public Optional<commons.Debt> deleteDebtById(long id) {
+        if (!debtRepository.existsById(id)) {
+            return Optional.empty();
+        }
+        debtRepository.deleteById(id);
+        return Optional.of(new Debt());
+    }
+
+    /**
+     * adds a new debt to the repository
+     * @param debt debt to add
+     * @return an Optional containing the added debt if successfully added, otherwise empty
+     */
+    public Optional<commons.Debt> addDebt(commons.Debt debt) {
+        if (debt.getDebtor() == null || debt.getCreditor() == null
+                || debt.getAmount() == null) {
+            return Optional.empty();
+        }
+        return Optional.of(debtRepository.save(debt));
+    }
+
+    /**
+     * gets a random debt from the repository
+     * @return an Optional containing a randomly selected debt if available, otherwise empty
+     */
+    public Optional<commons.Debt> getRandomDebt() {
+        java.util.List<commons.Debt> debts = debtRepository.findAll();
+        if (debts.isEmpty()) {
+            return Optional.empty();
+        }
+        int index = new Random().nextInt(debts.size());
+        return Optional.of(debts.get(index));
+    }
 }
