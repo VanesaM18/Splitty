d7b3fe04d36c2cb36a6743a982fc213d78f285f5
Andreea Grigoraș <A.Grigoras-1@student.tudelft.nl>
2024-03-29 18:27:28 +0000

    add server-side components for domain model

--- server/src/main/java/server/DomainValidationController.java
+++ server/src/main/java/server/DomainValidationController.java
@@ -1,13 +1,16 @@
 package server;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.util.UUID;
+
 @Controller
 @RequestMapping("/")
-public class SomeController {
+public class DomainValidationController {
 
     /**
      * Used for testing purposes for now
@@ -17,5 +20,19 @@ public class SomeController {
     @ResponseBody
     public String index() {
         return "Hello world!";
+    }
+
+
+    @Autowired
+    private UUID domainModelUuid;
+
+    /**
+     * validates the domain and returns its UUID as a string.
+     * @return UUID of the domain model as a string.
+     */
+    @GetMapping("/splitty-domain")
+    @ResponseBody
+    public String validate() {
+        return domainModelUuid.toString();
     }
 }
